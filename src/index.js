b38fb486666e7073a0b8c67c9ff001a7a5f529dc
Shoaib Ansari <shoaib.ahmad.ansari@accenture.com>
2025-03-19 20:42:43 +0530

    initial push

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const element = React.createElement('div',{}, 'I am div');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <element />
   </React.StrictMode>
 );
 
